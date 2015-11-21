64f28725b27d574da7abece880f55f9feb17de0b
Christopher Stott <christopherstott@gmail.com>
2015-11-20 22:01:50 -0800

    Initial Commit

---
+++ Octobird/Octobird.h
@@ -0,0 +1,19 @@
+//
+//  Octobird.h
+//  Octobird
+//
+//  Created by Chris on 2015-11-20.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Octobird.
+FOUNDATION_EXPORT double OctobirdVersionNumber;
+
+//! Project version string for Octobird.
+FOUNDATION_EXPORT const unsigned char OctobirdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Octobird/PublicHeader.h>
+
+
